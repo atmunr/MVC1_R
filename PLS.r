69e1b9307e34e156b82d51b054ed7e51ed6dcc83
atmunr <atmunr@gmail.com>
2019-06-02 12:34:38 -0300

    Cambia nombres de cosas y agrega gráficas.

--- PLS.r
+++ PLS.r
@@ -1,91 +1,91 @@
 # centra un vector
-centrarVector = function( V ) {
-  meanval = mean(V)
-  V_cent = V - meanval
-  return(list( V_cent, meanval ))
+CentrarVector = function( vector ) {
+  vector.prom = mean( vector )
+  vector.cent = vector - vector.prom
+  return(list( vector.cent, vector.prom ))
 }
 
 # centra una matriz 2D por columnas
-centrarMatriz_2_2 = function( M ) {
+CentrarMatriz2DPorColumnas = function( matriz ) {
   # columna promedio de M: el i-ésimo valor de column_prom es el promedio de
   # los valores de la i-ésima fila de M
-  col_mean = matrix( nrow = nrow(M) )
-  for ( i in 1 : nrow(M) ) {
-    col_mean[i] = mean( M[i,] )
+  colprom = matrix( nrow = nrow( matriz ) )
+  for ( i in 1 : nrow( matriz ) ) {
+    colprom[i] = mean( matriz[i,] )
   }
   # matriz centrada: a cada columna se le resta la columna promedio
-  M_cent = matrix( nrow = nrow(M), ncol = ncol(M) )
-  for ( i in 1 : ncol(M_cent) ) {
-    M_cent[,i] = M[,i] - col_mean
+  matriz.cent = matrix( nrow = nrow( matriz ), ncol = ncol( matriz ) )
+  for ( i in 1 : ncol(matriz.cent) ) {
+    matriz.cent[,i] = matriz[,i] - colprom
   }
-  return(list( M_cent, col_mean ))
+  return(list( matriz.cent, colprom ))
 }
 
 # produce coeficientes de regresión
 # para unas muestras de calibrado y un número variables latentes
 # usando el modelo PLS
-PLS = function( Xcal, Ycaln, var_lat ) {
-  I = ncol(Xcal) # n° de muestras en Xcal
-  J = nrow(Xcal) # orden de una muestra
+CalcularCoefRegrPLS = function( calib.x, calib.y, num.var.lat ) {
+  I = ncol( calib.x ) # n° de muestras en Xcal
+  J = nrow( calib.x ) # orden de una muestra
   w = matrix(         , ncol = J       )
-  v = matrix(         , ncol = var_lat )
-  W = matrix( nrow = J, ncol = var_lat )
-  T = matrix( nrow = I, ncol = var_lat )
-  P = matrix( nrow = J, ncol = var_lat )
+  v = matrix(         , ncol = num.var.lat )
+  W = matrix( nrow = J, ncol = num.var.lat )
+  T = matrix( nrow = I, ncol = num.var.lat )
+  P = matrix( nrow = J, ncol = num.var.lat )
 
-  for ( i in 1 : var_lat ) {
-    w     = Xcal %*% Ycaln / as.numeric(t(Ycaln) %*% Ycaln)
+  for ( i in 1 : num.var.lat ) {
+    w     = calib.x %*% calib.y / as.numeric( t( calib.y ) %*% calib.y )
     W[,i] = w / as.numeric(sqrt( t(w) %*% w ))
-    T[,i] = t(Xcal) %*% W[,i]
-    v[i]  = t(T[,i]) %*% Ycaln / as.numeric(t(T[,i]) %*% T[,i])
-    P[,i] = Xcal %*% T[,i] / as.numeric(t(T[,i]) %*% T[,i])
-    Xcal  = Xcal - P[,i] %*% t(T[,i])
-    Ycaln = Ycaln - v[i] * T[,i]
+    T[,i] = t( calib.x ) %*% W[,i]
+    v[i]  = t(T[,i]) %*% calib.y / as.numeric(t(T[,i]) %*% T[,i])
+    P[,i] = calib.x %*% T[,i] / as.numeric(t(T[,i]) %*% T[,i])
+    calib.x  = calib.x - P[,i] %*% t(T[,i])
+    calib.y = calib.y - v[i] * T[,i]
   }
-  coef_regr  = W %*% solve(t(P) %*% W) %*% t(v)
-  return( coef_regr ) # coeficientes de regresión
+  coef.regr  = W %*% solve(t(P) %*% W) %*% t(v)
+  return( coef.regr ) # coeficientes de regresión
 }
 
 # prueba todos los números de variables latentes desde 1 a var_lat_max
 # y devuelve su error estadístico PRESS por validación cruzada
-errorPorNumVarLat = function( Xcal, Ycaln, var_lat_max, centrar ) {
+CalcularPRESSPorNumVarLat = function( calib.x, calib.y, num.max.var.lat, centrar.datos ) {
 
   # salida, lista de errores PRESS, de uno a var_lat_max
-  press_vals = numeric( var_lat_max )
+  press.vals = numeric( num.max.var.lat )
 
-  for ( var_lat in 1 : var_lat_max ) { # para todos los números de variables latentes
-    for ( i in 1 : ncol(Xcal) ) { # para todas las muestras
+  for ( num.var.lat in 1 : num.max.var.lat ) { # para todos los números de variables latentes
+    for ( i in 1 : ncol( calib.x ) ) { # para todas las muestras
       # se aisla la i-ésima muestra
-      xout   = Xcal[,i]
-      Xcalp  = Xcal[,-i]
-      yout   = Ycaln[i]
-      Ycalnp = Ycaln[-i]
+      muestra.asilada.x   = calib.x[,i]
+      calib.x.p  = calib.x[,-i]
+      muestra.asilada.y   = calib.y[i]
+      calib.y.p = calib.y[-i]
 
-      if ( centrar == TRUE ) { # se centran los datos
-        datos_centrados_X = centrarMatriz_2_2( Xcalp )
-        Xcalp             = datos_centrados_X[[1]]
-        Xcalpmean         = datos_centrados_X[[2]]
-        datos_centrados_Y = centrarVector( Ycalnp )
-        Ycalnp            = datos_centrados_Y[[1]]
-        Ycalnpmean        = datos_centrados_Y[[2]]
-        xout = xout - Xcalpmean
-        yout = yout - Ycalnpmean
+      if ( centrar.datos == TRUE ) { # se centran los datos
+        datos_centrados_X = CentrarMatriz2DPorColumnas( calib.x.p )
+        calib.x.p             = datos_centrados_X[[1]]
+        calib.x.p.prom         = datos_centrados_X[[2]]
+        datos_centrados_Y = CentrarVector( calib.y.p )
+        calib.y.p            = datos_centrados_Y[[1]]
+        calib.y.p.prom        = datos_centrados_Y[[2]]
+        muestra.asilada.x = muestra.asilada.x - calib.x.p.prom
+        muestra.asilada.y = muestra.asilada.y - calib.y.p.prom
       }
 
       # se crea un modelo PLS con los datos, se intenta predecir el valor de la
       # muestra aislada y se suma el error cuadrado al PRESS de este número de
       # variables latentes
-      coef_regr = as.vector(PLS( Xcalp, Ycalnp, var_lat ))
-      yout_pred = coef_regr %*% xout
-      press_vals[var_lat] = press_vals[var_lat] + ((yout_pred - yout)^2)
+      coef.regr = as.vector(CalcularCoefRegrPLS( calib.x.p, calib.y.p, num.var.lat ))
+      muestra.asilada.y.pred = coef.regr %*% muestra.asilada.x
+      press.vals[num.var.lat] = press.vals[num.var.lat] + ((muestra.asilada.y.pred - muestra.asilada.y)^2)
     }
   }
-  return( press_vals )
+  return( press.vals )
 }
 
 # devuelve la probablidad de obtener la estadística F
 # en función de F y los grados de libertad k1 y k2
-probFstat = function( f, k1, k2 ) {
+CalcularProbF = function( f, k1, k2 ) {
   d1 = 0.0498673470
   d2 = 0.0211410061
   d3 = 0.0032776263
@@ -109,29 +109,29 @@ probFstat = function( f, k1, k2 ) {
 }
 
 # calcula el número óptimo de variables latentes a utilizar entre 1 y var_lat_max
-numOptimoVarLat = function( Xcal, Ycaln, var_lat_max, centrar ) {
+CalcularNumOptVarLat = function( calib.x, calib.y, num.max.var.lat, centrar.datos ) {
   # errores PRESS de distintos número de variables latentes
-  press_vals = errorPorNumVarLat( Xcal, Ycaln, var_lat_max, centrar )
+  press.vals = CalcularPRESSPorNumVarLat( calib.x, calib.y, num.max.var.lat, centrar.datos )
   # estadísticas F para los errores PRESS
-  f_vals = press_vals / press_vals[ length(press_vals) ]
+  f.vals = press.vals / press.vals[ length( press.vals ) ]
   # probabilidad P de obtener cada estadística F
-  p_vals = probFstat( f_vals, ncol(Xcal), ncol(Xcal) )
+  p.vals = CalcularProbF( f.vals, ncol( calib.x ), ncol( calib.x ) )
   # teorema: el número óptimo de variables latentes es el primero para el cual
   # la probabilidad  P es menor a 0.75
-  for ( i in 1 : length(p_vals) ) {
-    if( p_vals[i] < 0.75 ) return(i)
+  for ( i in 1 : length( p.vals ) ) {
+    if( p.vals[i] < 0.75 ) return(i)
   }
   return(-1)
 }
 
-Xcal  = as.matrix(read.table("tests/Xcal.txt" )) # espectros de calibrado
-Ycal1 = as.matrix(read.table("tests/Ycal1.txt")) # concentraciones de calibrado del primer analito
-Xtest = as.matrix(read.table("tests/Xtest.txt")) # espectros de predicción
+calib.x = as.matrix(read.table("tests/Xcalib.txt" )) # espectros de calibrado
+calib.y = as.matrix(read.table("tests/Ycalib.txt" )) # concentraciones de calibrado del primer analito
+test.x  = as.matrix(read.table("tests/Xtest.txt"  )) # espectros de predicción
 
-A = numOptimoVarLat( Xcal, Ycal1, 10, centrar = TRUE ) # variables latentes
+num.var.lat = CalcularNumOptVarLat( calib.x, calib.y, 10, centrar.datos = TRUE ) # variables latentes
 
-bn = PLS( Xcal, Ycal1, A ) # coeficientes de regresión
+coef.regr = CalcularCoefRegrPLS( calib.x, calib.y, num.var.lat ) # coeficientes de regresión
 
-Ytest1 = t(Xtest) %*% bn # concentraciones predichas del primer analito
+test.y = t( test.x ) %*% coef.regr # concentraciones predichas del primer analito
 
-write.table( Ytest1, file = "tests/Ytest1.txt", row.names = FALSE, col.names = FALSE )
+write.table( test.y, file = "tests/Ytest.txt", row.names = FALSE, col.names = FALSE )
--- app.R
+++ app.R
@@ -32,10 +32,16 @@ headerPanel("Calibración Multivariada"),
                      "Prueba X" = "test.x"  ,
                      "Prueba Y" = "test.y"
 
-            )),
-            tableOutput( "vis.crudo.out" )
+            )), tableOutput( "vis.crudo.out" )
         ),
-        tabPanel( "Gráficas" )
+        tabPanel( "Gráficas",
+			selectInput( "vis.grafica", "Mostrar datos:", c(
+				"Calibración X" = "calib.x" ,
+				"Calibración Y" = "calib.y" ,
+					 "Prueba X" = "test.x"  ,
+					 "Prueba Y" = "test.y"
+			)), plotOutput( "vis.grafica.out" )
+		)
     ))
 )
 
@@ -49,6 +55,37 @@ server <- function( input, output ) {
         if ( is.null(vis.crudo.val.file) ) { vis.crudo.val <- NULL }
         else { vis.crudo.val <- read.table( vis.crudo.val.file$datapath ) }
         output$vis.crudo.out <- renderTable({ vis.crudo.val })
+    })
+
+	observe({
+        if( input$vis.grafica == "calib.x" ) { vis.grafica.val.file <- input$calib.x }
+        if( input$vis.grafica == "calib.y" ) { vis.grafica.val.file <- input$calib.y }
+        if( input$vis.grafica ==  "test.x" ) { vis.grafica.val.file <- input$test.x  }
+        if( input$vis.grafica ==  "test.y" ) { vis.grafica.val.file <- input$test.y  }
+        if ( is.null(vis.grafica.val.file) ) { vis.grafica.val <- NULL }
+		else { vis.grafica.val <- read.table( vis.grafica.val.file$datapath ) }
+		output$vis.grafica.out <- renderPlot({
+
+			if( input$vis.grafica == "calib.x" || input$vis.grafica == "test.x" ) {
+				matplot( 1 : nrow(vis.grafica.val), vis.grafica.val,
+					xlab = 'Espectro', ylab = 'Absorbancia',
+					lwd = 1.5, type = 'l' )
+				par( xpd = TRUE )
+				legend( 'bottom', inset = 1,
+					legend = sprintf("%s", seq( 1 : ncol(vis.grafica.val) )),
+					horiz = TRUE, fill = c( 1 : ncol(vis.grafica.val) ) )
+			}
+			if( input$vis.grafica == "calib.y" || input$vis.grafica == "test.y" ) {
+				plot( 1 : nrow(vis.grafica.val), vis.grafica.val[,1],
+					xlab = 'N° de Muestra', ylab = 'Contenido',
+			 		bg = c( 1 : nrow(vis.grafica.val) ), pch = 21 )
+				par( xpd = TRUE )
+				legend( 'bottom', inset = 1,
+					legend = sprintf("%s", seq( 1 : nrow(vis.grafica.val) )),
+					horiz = TRUE, fill = c( 1 : nrow(vis.grafica.val) ) )
+			}
+
+		})
     })
 
 }
